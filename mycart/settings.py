8c93277f2af9c62ea1853d83bcdf648757901db9
Piyal Hossein <piyal13133@gmail.com>
2023-12-27 21:25:26 +0600

    Hello

--- mycart/settings.py
+++ mycart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hdoyt4cmxtcx)vz60ua%)+m)+$h0=zs9u1w+mh07w*!%h@6po@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 SITE_ID = 1
 
