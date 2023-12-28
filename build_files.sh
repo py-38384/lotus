31aabda651e8e55d34932036ebd28e99501411bc
Piyal Hossein <piyal13133@gmail.com>
2023-12-28 19:36:40 +0600

    selected vercel for test hosting

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,11 @@
-# pip install -r requirements.txt
-# python3.9 manage.py collectstatic
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
 
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+# set -o errexit
 
-poetry install
+# poetry install
 
-python manage.py collectstatic
-python manage.py migrate
+# python manage.py collectstatic
+# python manage.py migrate
